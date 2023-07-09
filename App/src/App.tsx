a1db0372f502aba8269480e7045cf52366d91260
Connor-Schulz <schulz_connor@yahoo.com>
2023-07-09 02:09:38 -0400

    update App

--- App/src/App.tsx
+++ App/src/App.tsx
@@ -1,35 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Move } from "./pages/Move";
+import { Navbar } from "./components/Navbar";
+import { createGlobalStyle } from "styled-components";
+import styled from "styled-components";
+import { Product } from "./pages/Product";
+import Styles from "./Styles";
+
+const Body = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: ${Styles.fontFamily};
+  }
+
+  &::selection {
+    background: ${Styles.primary2};
+    color: ${Styles.white1};
+  }
+`;
+
+const AppBody = styled.div`
+  display: grid;
+  grid-template-columns: auto 1fr;
+  grid-template-rows: 50px 1fr;
+  grid-template-areas:
+    "header  header"
+    "sidebar content";
+  height: 100vh;
+  width: 100vw;
+  background-color: #121212;
+`;
+
+const MainDisplay = styled.div`
+  grid-area: content;
+  padding-left: 50px;
+  overflow-y: auto;
+  margin: 10px;
+
+  &::-webkit-scrollbar {
+    width: 15px;
+  }
+
+  &::-webkit-scrollbar-track {
+    border-radius: 8px;
+    background: #201e38;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background: #8a88f7;
+    border-radius: 8px;
+    position: absolute;
+    left: -10px;
+
+    &:hover {
+      background: #7275f5;
+    }
+  }
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Body />
+      <AppBody>
+        <Navbar />
+        <MainDisplay className="main-display" id="main-display">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/move" element={<Move />} />
+            <Route path="/product" element={<Product />} />
+          </Routes>
+        </MainDisplay>
+      </AppBody>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
