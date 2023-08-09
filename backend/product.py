1d1143adf2322bda234126cf347f350727468424
Connor-Schulz <schulz_connor@yahoo.com>
2023-08-09 19:50:18 -0400

    initialize product class

---
+++ backend/product.py
@@ -0,0 +1,9 @@
+
+
+class Product:
+    def __init__(self, name, desc, price, quantity, id):
+        self.name = name
+        self.desc = desc
+        self.price = price
+        self.quantity = quantity
+        self.id = id
